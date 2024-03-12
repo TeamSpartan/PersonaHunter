80b730393fdaf76d95f33891da6d863efc940053
Yusuke17 <yuusuke.k1717@gmail.com>
2024-03-12 20:11:57 +0900

    [fix]プレイヤーHPが減るたびに死んでたのを修正

--- Assets/GameScripts/Scripts/Player/Scripts/PlayerHp.cs
+++ Assets/GameScripts/Scripts/Player/Scripts/PlayerHp.cs
@@ -2,13 +2,20 @@ using UnityEngine;
 
 public class PlayerHp : MonoBehaviour, IDamagedComponent
 {
-	[SerializeField, Header("初期HP")] private int maxHp;
+	[SerializeField, Header("初期HP")] private int initialHp;
 	[SerializeField, Header("無敵フレーム")] private int invincibilityFrame;
-	private System.Action _onDeath, _onReceiveDamage, _onReceiveHeal, _onHitWhileInvulnerable, _onBecomeVulnerable, _onResetDamage;
+
+	private System.Action _onDeath,
+		_onReceiveDamage,
+		_onReceiveHeal,
+		_onHitWhileInvulnerable,
+		_onBecomeVulnerable,
+		_onResetDamage;
+
 	private int _currentHp;
 	private bool _isInvulnerable;
 	private int _frameSinceLastHit;
-	
+
 	private void Start()
 	{
 		ResetDamage();
@@ -16,6 +23,12 @@ public class PlayerHp : MonoBehaviour, IDamagedComponent
 
 	private void OnEnable()
 	{
+		_onResetDamage += () => Debug.Log("ResetDamage");
+		_onReceiveDamage += () => Debug.Log("ReceiveDamage");
+		_onBecomeVulnerable += () => Debug.Log("BecomeVulnerable");
+		_onReceiveHeal += () => Debug.Log("ReceiveHeal");
+		_onHitWhileInvulnerable += () => Debug.Log("HitWhileInvulnerable");
+		_onDeath+= () => Debug.Log("Death");
 	}
 
 	private void OnDisable()
@@ -27,12 +40,15 @@ public class PlayerHp : MonoBehaviour, IDamagedComponent
 		//無敵時間中
 		if (_isInvulnerable)
 		{
-			++_frameSinceLastHit;
 			if (_frameSinceLastHit > invincibilityFrame)
 			{
 				_frameSinceLastHit = 0;
 				_isInvulnerable = false;
-				_onBecomeVulnerable.Invoke();
+				_onBecomeVulnerable?.Invoke();
+			}
+			else
+			{
+				++_frameSinceLastHit;
 			}
 		}
 	}
@@ -41,34 +57,40 @@ public class PlayerHp : MonoBehaviour, IDamagedComponent
 	///<summary>HPのリセット</summary>
 	public void ResetDamage()
 	{
-		_currentHp = maxHp;
-		_onReceiveDamage.Invoke();
+		_currentHp = initialHp;
+		_onResetDamage?.Invoke();
 	}
 
 	public void AddDamage(float dmg)
 	{
-		_currentHp -= (int)dmg;
-		if (dmg < 0)
+		if (_currentHp <= 0)
 		{
-			//Processing when HP decreases
-			_onReceiveDamage.Invoke();
-			_isInvulnerable = true;
-		}
-		else
-		{
-			//Processing when HP increases
-			_onReceiveHeal.Invoke();
+			return;
 		}
 
 		//Received damage while invincible
 		if (_isInvulnerable)
 		{
-			_onHitWhileInvulnerable.Invoke();
+			_onHitWhileInvulnerable?.Invoke();
+			return;
 		}
-		
+
+		_currentHp -= (int)dmg;
+		if (dmg > 0)
+		{
+			//Processing when HP decreases
+			_onReceiveDamage?.Invoke();
+			_isInvulnerable = true;
+		}
+		else
+		{
+			//Processing when HP increases
+			_onReceiveHeal?.Invoke();
+		}
+
 		//at time of death
-		if(_currentHp >= 0)
-			_onDeath.Invoke();
+		if (_currentHp <= 0)
+			_onDeath?.Invoke();
 	}
 
 	public void Kill()
