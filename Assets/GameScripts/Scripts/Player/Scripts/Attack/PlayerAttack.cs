a1b5c3b4fc8159f1c0baef0e58dc1732c4018a7b
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-04-04 23:36:57 +0900

    [add]プレイヤーの攻撃処理を追加

---
+++ Assets/GameScripts/Scripts/Player/Scripts/Attack/PlayerAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    //コンボアクション用
+    public bool IsAttack; //攻撃用アクションフラグ
+    private int _comboCount; //コンボ判定用フラグ
+    private string _currentName; //モーション名
+    [SerializeField, Tooltip("")]
+
+    /// <summary>
+    /// 攻撃時に呼ばれる処理
+    /// </summary>
+    public void Attack()
+    {
+        if(_comboCount < 2)
+        {
+            if(_currentName == null )
+            {
+                
+            }
+            else if()
+        }
+    }
+
+    /// <summary>
+    /// アタックフラグのリセットメソッド
+    /// </summary>
+    public void AttackClear()
+    {
+        IsAttack = false;
+    }
+
+    /// <summary>
+    /// コンボフラグをリセットメソッド
+    /// </summary>
+    public void ClearCombo()
+    {
+        _comboCount = 0;
+    }
+}
