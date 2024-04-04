f5dc1c1613255c36cb0a01c8b82389cadb4cf88b
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-04-05 01:01:03 +0900

    [update] ぬゑBT４割

--- Assets/AIBehaviours/BOSSBehaviours/NueBT.cs
+++ Assets/AIBehaviours/BOSSBehaviours/NueBT.cs
@@ -1,6 +1,7 @@
 using SgLibUnite.BehaviourTree;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 // 作成 菅沼
 [RequireComponent(typeof(NavMeshAgent))]
@@ -19,8 +20,8 @@ public class NueBT
     [SerializeField, Header("Flinch Threshold")]
     private float _flinchThreshold;
 
-    [SerializeField, Header("Awaiting Time When Get Parry[sec]")]
-    private float _awaitOnParry;
+    [FormerlySerializedAs("_awaitOnParry")] [SerializeField, Header("Awaiting Time When Get Parry[sec]")]
+    private float _awaitOnStumble;
 
     [SerializeField, Header("Awaiting Time When Get Flinch[sec]")]
     private float _awaitOnFlinching;
@@ -28,8 +29,16 @@ public class NueBT
     [SerializeField, Range(1f, 100f), Header("Sight Range")]
     private float _sightRange;
 
-    [SerializeField, Range(1f, 100f), Header("Attacking Range")]
-    private float _attackingRange;
+    [Header("Attacking Range")] [SerializeField, Range(1f, 100f)]
+    private float _clawAttackRange;
+
+    [SerializeField, Range(5f, 100f)] private float _taleAttackRange;
+    [SerializeField, Range(10f, 100f)] private float _rushAttackRange;
+
+    [SerializeField, Header("Player LayerMask")]
+    private LayerMask _playerLayers;
+
+    [SerializeField, Header("Player Tag")] private string _playerTag;
 
     #endregion
 
@@ -37,7 +46,7 @@ public class NueBT
 
     private BTBehaviour _btbIdle;
     private BTBehaviour _btbGetClose;
-    private BTBehaviour _btbSelectAttackType;
+    private BTBehaviour _btbCheckAttackType;
     private BTBehaviour _btbClaw;
     private BTBehaviour _btbTale;
     private BTBehaviour _btbRush;
@@ -47,43 +56,151 @@ public class NueBT
 
     #endregion
 
-    private BehaviourTree _behaviourTree;
+    #region Conditions
+
+    private bool _clawAttackable;
+    private bool _taleAttackable;
+    private bool _rushable;
+
+    #endregion
+
+    #region Transition Name
+
+    private string _bttIdleToClose = "01";
+    private string _bttCloseToCheckAtkType = "02";
+    private string _bttStartClaw = "03";
+    private string _bttStartTale = "04";
+    private string _bttStartRush = "05";
+
+    #endregion
+
+    #region Timers
+
+    private float _flinchingETime;
+    private float _stumblingETime;
+
+    #endregion
+
+    private BehaviourTree _bt;
     private Transform _player;
     private NavMeshAgent _agent;
     private Animator _animator;
+    private float _flinchValue;
 
     #region States
 
-    private void SelectAttackBehaviour()
+    private void Stay()
     {
-        
+        if (_agent.destination != transform.position)
+        {
+            _agent.ResetPath();
+            _agent.SetDestination(transform.position);
+        }
+    }
+
+    private void GetCloseToPlayer()
+    {
+        if (_agent.destination != _player.position)
+        {
+            _agent.SetDestination(_player.position);
+        }
+    }
+
+    private void CheckAttackingType()
+    {
+        // 範囲内かまず判定
+        _clawAttackable = Physics.CheckSphere(transform.position, _clawAttackRange, _playerLayers);
+        _taleAttackable = Physics.CheckSphere(transform.position, _taleAttackRange, _playerLayers);
+        _rushable = Physics.CheckSphere(transform.position, _rushAttackRange, _playerLayers);
+
+        // 前後判定を条件に上乗せ
+        var vt = (_player.position - transform.position).normalized;
+        var frontCond = Vector3.Dot(vt, transform.forward) >= 0;
+
+        _clawAttackable = _clawAttackable && frontCond;
+        _taleAttackable = _taleAttackable && !frontCond;
     }
 
     private void Claw()
     {
+        _agent.ResetPath();
     }
 
     private void Tale()
     {
+        _agent.ResetPath();
     }
 
     private void Rush()
     {
+        _agent.ResetPath();
     }
 
     private void Death()
     {
+        _agent.ResetPath();
     }
 
     private void Flinch()
     {
+        _flinchingETime += Time.deltaTime;
+        if (_flinchingETime > _awaitOnFlinching)
+        {
+        }
     }
 
     private void Stumble()
     {
+        _stumblingETime += Time.deltaTime;
+        if (_stumblingETime > _awaitOnStumble)
+        {
+        }
     }
 
     #endregion
+
+    private void SetupBehaviours()
+    {
+        _btbIdle = new();
+        _btbGetClose = new();
+        _btbCheckAttackType = new();
+        _btbClaw = new();
+        _btbTale = new();
+        _btbRush = new();
+        _btbDeath = new();
+        _btbFlinch = new();
+        _btbStumble = new();
+
+        _btbIdle.AddBehaviour(Stay);
+        _btbGetClose.AddBehaviour(GetCloseToPlayer);
+        _btbCheckAttackType.AddBehaviour(CheckAttackingType);
+        _btbClaw.AddBehaviour(Claw);
+        _btbTale.AddBehaviour(Tale);
+        _btbRush.AddBehaviour(Rush);
+        _btbDeath.AddBehaviour(Death);
+        _btbFlinch.AddBehaviour(Flinch);
+        _btbStumble.AddBehaviour(Stumble);
+    }
+
+    private void SetupTransition()
+    {
+        _bt.MakeTransition(_btbIdle, _btbGetClose, _bttIdleToClose);
+        _bt.MakeTransition(_btbGetClose, _btbCheckAttackType, _bttCloseToCheckAtkType);
+        _bt.MakeTransition(_btbCheckAttackType, _btbClaw, _bttStartClaw);
+        _bt.MakeTransition(_btbCheckAttackType, _btbTale, _bttStartTale);
+        _bt.MakeTransition(_btbCheckAttackType, _btbRush, _bttStartRush);
+    }
+
+    private void SetupBT()
+    {
+        SetupBehaviours();
+        SetupTransition();
+        _bt.ResistBehaviours(new[]
+        {
+            _btbIdle, _btbGetClose, _btbCheckAttackType, _btbClaw, _btbTale, _btbRush, _btbDeath, _btbFlinch,
+            _btbStumble
+        });
+    }
 
     public float GetHealth()
     {
@@ -97,6 +214,21 @@ public class NueBT
 
     public void InitializeThisComponent()
     {
+        _bt = new();
+
+        _agent = GetComponent<NavMeshAgent>();
+
+        if (GetComponent<Animator>() != null)
+        {
+            _animator = GetComponent<Animator>();
+        }
+        else if (GetComponentInChildren<Animator>() != null)
+        {
+            _animator = GetComponentInChildren<Animator>();
+        }
+        
+        SetupBT();
+        _bt.StartBT();
     }
 
     public void FinalizeThisComponent()
@@ -131,6 +263,10 @@ public class NueBT
         Gizmos.color = Color.cyan;
         Gizmos.DrawWireSphere(transform.position, _sightRange);
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, _attackingRange);
+        Gizmos.DrawWireSphere(transform.position, _clawAttackRange);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, _taleAttackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _rushAttackRange);
     }
 }
