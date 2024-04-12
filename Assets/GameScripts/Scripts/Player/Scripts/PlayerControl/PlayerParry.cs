7aff7d0bc1a2c652b00f5f8092dfb307c4aebdec
Yusuke17 <yuusuke.k1717@gmail.com>
2024-04-12 17:32:53 +0900

    [add]パリィ、移動、回避行動

---
+++ Assets/GameScripts/Scripts/Player/Scripts/PlayerControl/PlayerParry.cs
@@ -0,0 +1,80 @@
+﻿using Player.Input;
+using Player.Param;
+using UnityEngine;
+
+namespace Player.Action
+{
+	[RequireComponent(typeof(PlayerParam))]
+	[RequireComponent(typeof(Animator))]
+	///<summary>プレイヤーのパリー</summary>
+	public class PlayerParry : MonoBehaviour
+	{
+		public event System.Action OnParryStart,
+			OnParrySuccess,
+			OnEndParry,
+			OnDuringParry;
+
+		private int _parryID = Animator.StringToHash("IsParry");
+		private PlayerParam _playerParam;
+		private Animator _animator;
+
+		private void Start()
+		{
+			_playerParam = GetComponentInParent<PlayerParam>();
+			_animator = GetComponentInParent<Animator>();
+		}
+
+		private void Update()
+		{
+			if (PlayerInputsAction.Instance.GetCurrentInputType == PlayerInputTypes.Parry &&
+			    !_playerParam.GetIsAnimation)
+			{
+				Parried();
+			}
+		}
+
+		void Parried()
+		{
+			if (_playerParam.GetIsParry)
+			{
+				OnDuringParry?.Invoke();
+				return;
+			}
+
+			_playerParam.SetIsAnimation(true);
+			_animator.SetTrigger(_parryID);
+		}
+
+
+		///<summary>アニメーションイベントで呼び出す用</summary>------------------------------------------------------------------
+		public void Parry()
+		{
+			if (_playerParam.GetIsParry)
+			{
+				OnDuringParry?.Invoke();
+				return;
+			}
+
+			_playerParam.SetIsParry(true);
+			OnParryStart?.Invoke();
+		}
+
+		public void EndParry()
+		{
+			if (!_playerParam.GetIsParry)
+			{
+				return;
+			}
+
+			OnEndParry?.Invoke();
+			_playerParam.SetIsParry(false);
+		}
+
+		public void EndParryActions()
+		{
+			PlayerInputsAction.Instance.DeleteInputQueue(PlayerInputTypes.Parry);
+			_playerParam.SetIsAnimation(false);
+			PlayerInputsAction.Instance.EndAction();
+		}
+	}
+}
