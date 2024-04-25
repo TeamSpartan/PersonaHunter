749051d5e30aee9497c42a13a497cd96d20f166c
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-04-25 22:42:49 +0900

    [add]残像の処理を追加

---
+++ Assets/GameScripts/Scripts/AfterImage/AfterImageController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterImageController : AfterImageControllerBase
+{
+    [SerializeField, Header("残像の発生元となるオブジェクト")]
+    private Transform _originalTransform = null;
+    protected override void SetupParam()
+    {
+        _param = new SimpleAfterImageParam(_originalTransform);
+    }
+}
