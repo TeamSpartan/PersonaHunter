3533b2f320f7cddab1d346d3dfd0753df0fc9871
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-04-30 20:48:06 +0900

    [add]カメラオクルージョンとカメラシェイクを追加

---
+++ Assets/GameScripts/Scripts/CameraEffect/CameraOcculider/OccluderController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider), typeof(Rigidbody))]
+public class OccluderController : MonoBehaviour
+{
+    [SerializeField, Tooltip("どのくらい透明にするか"), Range(0f, 1f)] float _transparency = 0.2f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OccludeeController occuluController = other.gameObject.GetComponent<OccludeeController>();
+        if(occuluController)
+        {
+            occuluController.ChangeAlpha(_transparency);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OccludeeController occuluController = other.gameObject.GetComponent<OccludeeController>();
+        if(occuluController)
+        {
+            occuluController.ChangeAlphaOriginal();
+        }
+    }
+}
