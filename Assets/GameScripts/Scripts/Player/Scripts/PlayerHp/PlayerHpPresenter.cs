84994bbf0853e3cbbb1533ad7dbe88b3491e4f2d
Yusuke17 <yuusuke.k1717@gmail.com>
2024-05-20 23:41:12 +0900

    プレイヤーHpの作り方変更

---
+++ Assets/GameScripts/Scripts/Player/Scripts/PlayerHp/PlayerHpPresenter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace name
+{
+	///<summary>summary</summary>
+	public class PlayerHpPresenter : MonoBehaviour
+	{
+		private PlayerHpModel _playerHpModel;
+		private PlayerHpView _playerHpView;
+
+		private void Start()
+		{
+			_playerHpModel = GetComponent<PlayerHpModel>();
+			_playerHpView = GetComponent<PlayerHpView>();
+
+			_playerHpModel.OnReceiveDamage += _playerHpView.SetGauge;
+			_playerHpModel.OnRegeneration += _playerHpView.SetRegenerate;
+		}
+	}
+}
+
