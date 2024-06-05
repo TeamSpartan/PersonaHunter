fa548e3301f6206886daa3093ca3ac8757819a0b
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-06-05 17:54:36 +0900

    [update, ] マップ内歩けるとこまで

--- Assets/GameScripts/Scripts/Player/Scripts/PlayerControl/PlayerAvoid.cs
+++ Assets/GameScripts/Scripts/Player/Scripts/PlayerControl/PlayerAvoid.cs
@@ -5,127 +5,129 @@ using UnityEngine;
 
 namespace Player.Action
 {
-    [RequireComponent(typeof(PlayerParam))]
-    ///<summary>プレイヤーの回避</summary>
-    public class PlayerAvoid : MonoBehaviour, IInitializableComponent
-    {
-        [SerializeField] private Animator _animator;
-
-        private int _avoidId = Animator.StringToHash("IsAvoid");
-
-
-        public System.Action OnAvoidSuccess;
-        public System.Action<float> OnJustAvoidSuccess;
-
-        private PlayerParam _playerParam;
+	[RequireComponent(typeof(PlayerParam))]
+	///<summary>プレイヤーの回避</summary>
+	public class PlayerAvoid : MonoBehaviour, IInitializableComponent
+	{
+		
+		private int _avoidId = Animator.StringToHash("IsAvoid");
 
 
-        private void OnEnable()
-        {
-            OnAvoidSuccess += () => Debug.Log("回避成功");
-            OnJustAvoidSuccess += _ => Debug.Log("ジャスト回避成功");
-        }
+		public System.Action OnAvoidSuccess;
+		public System.Action<float> OnJustAvoidSuccess;
 
-        private void Start()
-        {
-            _playerParam = GetComponent<PlayerParam>();
-        }
+		private PlayerParam _playerParam;
+		private Animator _animator;
 
-        private void Update()
-        {
-            if (PlayerInputsAction.Instance.GetCurrentInputType == PlayerInputTypes.Avoid &&
-                !_playerParam.GetIsAnimation)
-            {
-                Avoided();
-            }
-        }
 
-        void Avoided()
-        {
-            if (_playerParam.GetIsAvoid)
-            {
-                return;
-            }
+		private void OnEnable()
+		{
+			OnAvoidSuccess += () => Debug.Log("回避成功");
+			OnJustAvoidSuccess += _ => Debug.Log("ジャスト回避成功");
+		}
 
-            _playerParam.SetIsAnimation(true);
-            _animator.SetTrigger(_avoidId);
-        }
+		private void Start()
+		{
+			_animator = GetComponent<Animator>();
+			_playerParam = GetComponent<PlayerParam>();
+		}
 
-        ///<summary>アニメーションイベントで呼び出す用</summary>------------------------------------------------------------------
-        public void Avoid()
-        {
-            if (_playerParam.GetIsAvoid)
-            {
-                return;
-            }
+		private void Update()
+		{
+			
+			if (PlayerInputsAction.Instance.GetCurrentInputType == PlayerInputTypes.Avoid && !_playerParam.GetIsAnimation)
+			{
+				Avoided();
+			}
+		}
 
-            _playerParam.SetIsAvoid(true);
-        }
+		void Avoided()
+		{
+			if (_playerParam.GetIsAvoid)
+			{
+				return;
+			}
 
-        public void JustAvoid()
-        {
-            if (_playerParam.GetIsJustAvoid)
-            {
-                return;
-            }
+			_playerParam.SetIsAnimation(true);
+			_animator.SetTrigger(_avoidId);
+		}
 
-            _playerParam.SetIsJustAvoid(true);
-        }
+		///<summary>アニメーションイベントで呼び出す用</summary>------------------------------------------------------------------
+		public void Avoid()
+		{
+			if (_playerParam.GetIsAvoid)
+			{
+				return;
+			}
 
-        public void EndAvoid()
-        {
-            if (!_playerParam.GetIsAvoid)
-            {
-                return;
-            }
+			_playerParam.SetIsAvoid(true);
+		}
 
-            _playerParam.SetIsAvoid(false);
-        }
+		public void JustAvoid()
+		{
+			if (_playerParam.GetIsJustAvoid)
+			{
+				return;
+			}
 
-        public void EndJustAvoid()
-        {
-            if (!_playerParam.GetIsJustAvoid)
-            {
-                return;
-            }
+			_playerParam.SetIsJustAvoid(true);
+		}
 
-            _playerParam.SetIsJustAvoid(false);
-        }
+		public void EndAvoid()
+		{
+			if (!_playerParam.GetIsAvoid)
+			{
+				return;
+			}
+			
+			_playerParam.SetIsAvoid(false);
+			
+		}
 
-        public void EndAvoidActions()
-        {
-            PlayerInputsAction.Instance.DeleteInputQueue(PlayerInputTypes.Avoid);
-            _playerParam.SetIsAnimation(false);
-            PlayerInputsAction.Instance.EndAction();
-        }
+		public void EndJustAvoid()
+		{
+			if (!_playerParam.GetIsJustAvoid)
+			{
+				return;
+			}
+			
+			_playerParam.SetIsJustAvoid(false);
+		}
 
-        //------------------------------------------------------------------------------------------------------------------------------------
-        public void InitializeThisComponent()
-        {
-        }
+		public void EndAvoidActions()
+		{
+			PlayerInputsAction.Instance.DeleteInputQueue(PlayerInputTypes.Avoid);
+			_playerParam.SetIsAnimation(false);
+			PlayerInputsAction.Instance.EndAction();
+		}
+		//------------------------------------------------------------------------------------------------------------------------------------
+		public void InitializeThisComponent()
+		{
+			
+		}
 
-        public void FixedTickThisComponent()
-        {
-            //throw new System.NotImplementedException();
-        }
+		public void FixedTickThisComponent()
+		{
+			//throw new System.NotImplementedException();
+		}
 
-        public void TickThisComponent()
-        {
-        }
+		public void TickThisComponent()
+		{
+		}
 
-        public void FinalizeThisComponent()
-        {
-            //throw new System.NotImplementedException();
-        }
+		public void FinalizeThisComponent()
+		{
+			//throw new System.NotImplementedException();
+		}
 
-        public void PauseThisComponent()
-        {
-            throw new NotImplementedException();
-        }
+		public void PauseThisComponent()
+		{
+			throw new NotImplementedException();
+		}
 
-        public void ResumeThisComponent()
-        {
-            throw new NotImplementedException();
-        }
-    }
+		public void ResumeThisComponent()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
