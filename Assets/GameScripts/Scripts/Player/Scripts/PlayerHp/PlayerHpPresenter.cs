a76602ddde1db2819da267c49968ac374181ed63
Yusuke17 <yuusuke.k1717@gmail.com>
2024-06-28 21:19:45 +0900

    残像実装、HPバーの画像入れ替え、アイドルアニメーションとLeftAttackが沈む問題を解消

--- Assets/GameScripts/Scripts/Player/Scripts/PlayerHp/PlayerHpPresenter.cs
+++ Assets/GameScripts/Scripts/Player/Scripts/PlayerHp/PlayerHpPresenter.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 ///<summary>summary</summary>
 public class PlayerHpPresenter : MonoBehaviour
 {
-    private PlayerHpModel _playerHpModel;
-    private PlayerHpView _playerHpView;
+	private PlayerHpModel _playerHpModel;
+	private PlayerHpView _playerHpView;
 
-    private void Start()
-    {
-        _playerHpModel = GetComponent<PlayerHpModel>();
-        _playerHpView = GetComponent<PlayerHpView>();
+	private void Start()
+	{
+		_playerHpModel = GetComponent<PlayerHpModel>();
+		_playerHpView = GetComponent<PlayerHpView>();
 
-        _playerHpModel.OnReceiveDamage += _playerHpView.SetGauge;
-        _playerHpModel.OnRegeneration += _playerHpView.SetRegenerate;
-    }
+		_playerHpModel.OnReceiveDamage += _playerHpView.SetGauge;
+		_playerHpModel.OnRegeneration += _playerHpView.SetRegenerate;
+		_playerHpModel.OnDie += _playerHpView.DisplayDiePanel;
+	}
 }
--- Assets/GameScripts/Scripts/Player/Scripts/PlayerHp/PlayerHpView.cs
+++ Assets/GameScripts/Scripts/Player/Scripts/PlayerHp/PlayerHpView.cs
@@ -1,5 +1,7 @@
-﻿using DG.Tweening;
+﻿using System.Collections;
+using DG.Tweening;
 using SgLibUnite.CodingBooster;
+using SgLibUnite.Systems;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,37 +10,56 @@ using UnityEngine.UI;
 /// </summary>
 public class PlayerHpView : MonoBehaviour
 {
-    [SerializeField, Header("HPが減る速度")] private float _duration;
-    [SerializeField, Header("赤ゲージが残る時間")] private float _waitTime = .2f;
-    [SerializeField] private Image healthImage;
-    [SerializeField] private Image burnImage;
+	[SerializeField, Header("HPが減る速度")] private float _duration;
+	[SerializeField, Header("赤ゲージが残る時間")] private float _waitTime = .2f;
+	[SerializeField, Header("死亡時のパネル")] private GameObject _diePanel;
 
-    private Tween _burnEffect;
+	[SerializeField, Header("死亡後何秒後にシーン移動するか")]
+	private float _dieWaitTime = 3f;
+	[SerializeField] private Image healthImage;
+	[SerializeField] private Image burnImage;
+	private SceneLoader _sceneLoader;
 
-    private void Start()
-    {
-        healthImage.fillAmount = 1f;
-        burnImage.fillAmount = 1f;
-    }
+	private Tween _burnEffect;
 
-    ///<summary>受ダメージ時にゲージを変化させる</summary>
-    ///<param name="InitialHp">初期HP</param>
-    ///<param name="CurrentHp"	>現在のHP</param>
-    public void SetGauge(float InitialHp, float CurrentHp)
-    {
-        _burnEffect?.Kill();
-        healthImage.DOFillAmount(CurrentHp / InitialHp, _duration).OnComplete(() =>
-        {
-            _burnEffect = burnImage.DOFillAmount(CurrentHp / InitialHp, _duration * 0.5f).SetDelay(_waitTime);
-        });
-    }
+	private void Start()
+	{
+		healthImage.fillAmount = 1f;
+		burnImage.fillAmount = 1f;
+		_diePanel?.SetActive(false);
+	}
 
-    ///<summary>リジェネ時にゲージを変化させる</summary>
-    ///<param name="InitialHp">初期HP</param>
-    ///<param name="CurrentHp"	>現在のHP</param>
-    public void SetRegenerate(float InitialHp, float CurrentHp)
-    {
-        healthImage.fillAmount = CurrentHp / InitialHp;
-        burnImage.fillAmount = CurrentHp / InitialHp;
-    }
+	///<summary>受ダメージ時にゲージを変化させる</summary>
+	///<param name="InitialHp">初期HP</param>
+	///<param name="CurrentHp"	>現在のHP</param>
+	public void SetGauge(float InitialHp, float CurrentHp)
+	{
+		_burnEffect?.Kill();
+		healthImage.DOFillAmount(CurrentHp / InitialHp, _duration).OnComplete(() =>
+		{
+			_burnEffect = burnImage.DOFillAmount(CurrentHp / InitialHp, _duration * 0.5f).SetDelay(_waitTime);
+		});
+	}
+
+	///<summary>リジェネ時にゲージを変化させる</summary>
+	///<param name="InitialHp">初期HP</param>
+	///<param name="CurrentHp"	>現在のHP</param>
+	public void SetRegenerate(float InitialHp, float CurrentHp)
+	{
+		healthImage.fillAmount = CurrentHp / InitialHp;
+		burnImage.fillAmount = CurrentHp / InitialHp;
+	}
+
+	public void DisplayDiePanel()
+	{
+		_diePanel?.SetActive(true);
+		StartCoroutine(WaitDie());
+	}
+
+	IEnumerator WaitDie()
+	{
+		yield return new WaitForSeconds(_dieWaitTime);
+		_sceneLoader = FindAnyObjectByType<SceneLoader>();
+		_sceneLoader.LoadSceneByName("Title");
+	}
 }
